b555632090467e9435c6b1dec8845f18cd1633aa
Omkar Joshi <omkarjoshiop@gmail.com>
2024-04-13 16:40:51 +0530

    Patterns

--- Patterns/pattern22.java
+++ Patterns/pattern22.java
@@ -4,11 +4,12 @@ import java.util.*;
 
 public class pattern22 {
 
-    // 1
-    // 2 1 2
-    // 3 2 1 2 3
-    // 4 3 2 1 2 3 4
-    // 5 4 3 2 1 2 3 4 5
+// 1 2 3 4 5 26 27 28 29 30
+//   6 7 8 9 22 23 24 25
+//     10 11 12 19 20 21
+//       13 14 17 18
+//         15 16
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
